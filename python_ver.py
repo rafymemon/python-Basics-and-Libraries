3bbe433ffcae539b7ba4be1e77e1386c3394938a
Rafy Mustafa <128308433+rafymemon@users.noreply.github.com>
2025-09-03 11:46:40 +0500

    checking pandas and numpy version

---
+++ python_ver.py
@@ -0,0 +1,5 @@
+import pandas
+import numpy
+
+print("Pandas version:", pandas.__version__)
+print("NumPy version:", numpy.__version__)
---
+++ python_version.py
@@ -0,0 +1,5 @@
+import pandas
+import numpy 
+
+print('Pandas version:', pandas.__version__)
+print('numpy version:', numpy.__version__)
